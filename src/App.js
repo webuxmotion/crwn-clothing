4ec624d0918274737a0be31d245cb7bfbcbbc19a
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-09-21 21:52:13 +0300

    withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Directory from './components/directory/directory.component';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
-    <Directory />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
