61cede5c1fba78a6f7d54f284df1f88440dc4c9a
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-09-29 22:23:23 +0300

    Use pedux-persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
-  user: userReducer,
-  cart: cartReducer,
-});
+export default persistReducer(
+  {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+  },
+  combineReducers({
+    user: userReducer,
+    cart: cartReducer,
+  }),
+);
