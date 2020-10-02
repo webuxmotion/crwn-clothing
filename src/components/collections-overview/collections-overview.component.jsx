a46581e15b2be1fdb58c7271344ef48620a834f3
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-10-02 19:48:28 +0300

    Nested routes

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import CollectionPreivew from '../../components/collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
 
-const CollectionOverview = ({ collections }) => (
-  <div className="collections-overview">
-    {
-      collections.map(({ id, ...rest }) => <CollectionPreivew key={id} {...rest} />)
-    }
-  </div>
-);
+const CollectionOverview = ({ collections }) => {
+  console.log('collections', collections);
+
+  return (
+    <div className="collections-overview">
+      {
+        collections.map(({ id, ...rest }) => <CollectionPreivew key={id} {...rest} />)
+      }
+    </div>
+  )
+};
 
 export default connect(
   createStructuredSelector({
-    collections: selectShopCollections
+    collections: selectCollectionsForPreview
   })
 )(CollectionOverview);
