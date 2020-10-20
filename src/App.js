1902826503efd333beb2474e13d6e4be645be27d
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-10-20 18:39:07 +0300

    useEffect instead of class based components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -16,37 +16,33 @@ import { checkUserSession } from './redux/user/user.actions';
 
 import './App.css';
 
-class App extends Component {
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
   
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/running' component={RunningPage} />
-          <Route 
-            exact 
-            path='/signin' 
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/running' component={RunningPage} />
+        <Route 
+          exact 
+          path='/signin' 
+          render={() =>
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInSignUpPage />
+            )
+          }
+        />
+      </Switch>
+    </div>
+  );
 }
 
 export default connect(
